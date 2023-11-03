2d1b49fb44ff423ae49e9d0383a2f0b8c4b57c78
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2023-11-03 17:15:43 +0900

    Add password_manager.sh
    Get passwordが入力され、サービス名保存時のif文の実装

--- password_manager.sh
+++ password_manager.sh
@@ -31,12 +31,24 @@ while true; do
     echo 'サービス名を入力してください：'
     read service_name
 
+    # 入力されたサービス名に一致する情報を取得
+    service_info=$(grep -E "^$service_name:" password_manager.txt)
+
     # サービス名が保存されていた場合
     if grep -q -E "^$service_name:" password_manager.txt; then
-      echo 'サービス名あり'
+        # サービス名、ユーザー名、パスワードを表示
+        service_name=$(echo "$service_info" | cut -d ":" -f 1)
+        username=$(echo "$service_info" | cut -d ":" -f 2)
+        password=$(echo "$service_info" | cut -d ":" -f 3)
+
+        echo "サービス名: $service_name"
+        echo "ユーザー名: $username"
+        echo "パスワード: $password"
+      # echo 'サービス名あり'
     # サービス名が保存されていなかった場合
     else
-      echo 'サービス名なし'
+      echo 'そのサービスは登録されていません。'
+      # echo 'サービス名なし'
     fi
       ;;
     # Exitが入力された場合
