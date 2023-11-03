d282c05fd37f15f0f486386a896941dd808e2640
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2023-11-03 16:00:06 +0900

    Add password_manager.sh
    選択肢の繰り返し表示の実装。

--- password_manager.sh
+++ password_manager.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
+# メッセージの表示
 echo 'パスワードマネージャーへようこそ！'
 
-#各情報の入力
-echo 'サービス名を入力してください：'
-read service_name
+# Exitが入力されるまでプログラムを終了せず選択肢を繰り返し表示
+while true; do
+  echo '次の選択肢から入力してください(Add Password/Get Password/Exit)：'
+  read choice
 
-echo 'ユーザー名を入力してください：'
-read user_name
+  # Add Passwordが入力された場合
+  case "$choice" in
+    "Add Password")
+      # 各情報の入力
+      echo 'サービス名を入力してください：'
+      read service_name
 
-echo 'パスワードを入力してください：'
-read password
+      echo 'ユーザー名を入力してください：'
+      read user_name
 
-#入力された情報を指定形式でファイルに保存
-echo "$service_name:$user_name:$password" >> password_manager.txt
+      echo 'パスワードを入力してください：'
+      read password
 
-echo 'Thank you!'
+      echo 'パスワードの追加は成功しました。'
+
+      # 入力された情報を指定形式でファイルに保存
+      echo "$service_name:$user_name:$password" >> password_manager.txt
+      ;;
+    # Get Passwordが入力された場合
+    "Get Password")
+    echo 'Get Passwordが出力されることの確認'
+      ;;
+    # Exitが入力された場合
+    "Exit")
+      echo 'Thank you!'
+      break
+      # プログラムの終了
+      ;;
+    # Add Password/Get Password/Exit 以外が入力された場合
+    *)
+      echo '入力が間違えています。Add Password/Get Password/Exit から入力してください。'
+      ;;
+  esac
+done
